2c66427c236ddf19ee6aaf209c42279aef6d122e
smlimonparvez <smlimonparvez@gmail.com>
2022-09-03 15:07:52 +0600

    first commit

---
+++ news.js
@@ -0,0 +1,86 @@
+const loadCategories = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    try {
+        fetch(url);
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCategories(data.data.news_category);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const displayCategories = (categories) => {
+    const newsCatagories = document.getElementById('new-catagory');
+    categories.forEach(category => {
+        // console.log(category);
+        const categoryItem = document.createElement('li');
+        categoryItem.classList.add('nav-item');
+        categoryItem.innerHTML = `
+        <a class="nav-link fw-bold p-2 ms-4" onclick= "loadNews(${category.category_id})" href="#">${category.category_name}</a>
+        `;
+        newsCatagories.appendChild(categoryItem);
+    });
+}
+
+const loadNews = async (id) => {
+    const url = ` https://openapi.programming-hero.com/api/news/category/${id}`;
+    try {
+        fetch(url);
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNews(data.data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const displayNews = (allNews) => {
+    const allNewsItem = document.getElementById('all-news-item');
+    allNews.forEach(news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.innerHTML = `
+        <div class="card mb-3 mx-5">
+        <div class="row g-0">
+            <div class="col-md-4 col-sm-12">
+                <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${news.title}</h5>
+                    <p class="card-text">${news.details.slice(0, 500)}</p>
+                    <div class="d-flex justify-content-around align-items-center">
+                        <div class="d-flex align-items-center justify-content-around">
+                            <img src="${news.author.img}" class="img-fluid rounded-circle" style="width:30px; height:30px;" alt="">
+                            <div class="ps-2 pt-3">
+                                <h6 class="m-0">${news.author.name ? news.author.name : "No name found"}</h6>
+                                <p>${news.author.published_date}</p>
+                            </div>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <i class="fa-regular fa-eye"></i>
+                            <p class="fw-bold m-0 ps-1">${news.total_view}</p>
+                        </div>
+                        <div class="">
+                            <i class="fa-sharp fa-solid fa-star" style="color: gray;"></i>
+                            <i class="fa-sharp fa-solid fa-star" style="color: gray;"></i>
+                            <i class="fa-sharp fa-solid fa-star" style="color: gray;"></i>
+                            <i class="fa-sharp fa-solid fa-star" style="color: gray;"></i>
+                            <i class="fa-regular fa-star-half-stroke" style="color: gray;"></i>
+                        </div>
+                        <div>
+                            <button id="show-details" class="btn btn-primary">Show Details</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        `;
+        allNewsItem.appendChild(newsDiv);
+    });
+}
+
+loadCategories();
